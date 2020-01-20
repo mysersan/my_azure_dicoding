07a85e34bdb58cce8f592925f5f12aca3944b9d9
mysersan <60083537+mysersan@users.noreply.github.com>
2020-01-20 16:43:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "<mysersan.database.windows.net>";
+    $user = "<mysersan>";
+    $pass = "<qweQWE123!@#>";
+    $db = "<dicodingdb>";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
